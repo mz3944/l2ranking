a1c8c6982f9d7d9963dde7159379f7003a66420d
Jure Cindro <jure.cindro@gmail.com>
2013-04-01 22:57:15 +0200

    Fixed validation

--- frontend/fields.py
+++ frontend/fields.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.core.exceptions import ValidationError
 from django.core.files.images import get_image_dimensions
 
-from l2ranking import settings as l2ranking_settings
+from l2ranking import settings
 
 
 class BannerField(models.ImageField):
@@ -11,20 +11,20 @@ class BannerField(models.ImageField):
     """
 
     description = "Banner image field with size validation"
-    __metaclass__ = models.SubfieldBase
 
     def clean(self, *args, **kwargs):
-        banner = super(BannerField, self).clean(*args, **kwargs)
+        data = super(BannerField, self).clean(*args, **kwargs)
+        banner = data.file
         if banner:
             width, heigth = get_image_dimensions(banner)
-            if banner._size > l2ranking_settings.BANNER_SIZE * 1024:
+            if banner._size > settings.BANNER_SIZE * 1024:
                 raise ValidationError(
-                    "Banner file too large ( > " + str(l2ranking_settings.BANNER_SIZE) + " kb )")
-            if width > l2ranking_settings.BANNER_WIDTH:
-                raise ValidationError("Banner too wide ( > " + str(l2ranking_settings.BANNER_WIDTH) + " px )")
-            if heigth > l2ranking_settings.BANNER_HEIGHT:
+                    "Banner file too large ( > " + str(settings.BANNER_SIZE) + " kb )")
+            if width > settings.BANNER_WIDTH:
+                raise ValidationError("Banner too wide ( > " + str(settings.BANNER_WIDTH) + " px )")
+            if heigth > settings.BANNER_HEIGHT:
                 raise ValidationError(
-                    "Banner too high (no pun, lol) ( > " + str(l2ranking_settings.BANNER_HEIGHT) + " px )")
-            return banner
+                    "Banner too high (no pun, lol) ( > " + str(settings.BANNER_HEIGHT) + " px )")
         else:
             raise ValidationError("Couldn't read the uploaded banner")
+        return data
